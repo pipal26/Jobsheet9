5b1ddedaab1184021ba54c0a72cb1d7cdf55cfbf
pipal26 <novalgg87@gmail.com>
2025-11-12 20:23:29 +0700

    Pertanyaan Percobaan 4

--- SearchNilai14.java
+++ SearchNilai14.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class SearchNilai14 {
     public static void main(String[] args) {
-        int[] arrNilai = {80, 85, 78, 96, 90, 82, 86};
-        int key = 90;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan banyaknya nilai yang akan diinput: ");
+        int n = sc.nextInt();
+        
+        int[] arrNilai = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": "); 
+            arrNilai[i] = sc.nextInt();
+        }
+
+        System.out.print("Masukkan nilai yang ingin dicari: ");
+        int key = sc.nextInt();
+        
         int hasil = 0;
 
         for (int i = 0; i < arrNilai.length; i++) {
@@ -11,8 +26,14 @@ public class SearchNilai14 {
             }
         }
 
-        System.out.println();
-        System.out.println("Nilai " + key + " ketemu di indeks ke-" + hasil);
-        System.out.println();
+        System.out.println(); 
+
+        if (hasil != 0){
+            System.out.println("Nilai " + key + " ketemu, merupakan nilai mahasiswa ke-" + (hasil + 1));
+        } else {
+            System.out.println("Nilai " + key + " tidak ditemukan.");
+        }
+        
+        sc.close();
     }
 }
---
+++ SearchNilai14.md
@@ -0,0 +1,12 @@
+1. Jelaskan maksud dari statement break; pada baris ke-10 kode program percobaan 4 di
+atas.
+menghentikan eksekusi loop secara paksa dan segera melompat keluar dari blok perulangan.
+
+2. Modifikasi kode program pada percobaan 4 di atas sehingga program dapat menerima
+input berupa banyaknya elemen array nilai, isi array, dan sebuah nilai (key) yang ingin
+dicari. Lalu cetak ke layar indeks posisi elemen dari nilai (key) yang dicari. Contoh hasil
+program:
+
+3. Modifikasi program pada percobaan 4 di atas, sehingga program akan memberikan pesan
+"Nilai yang dicari tidak ditemukan" jika nilai yang dicari (key) tidak ada di dalam array.
+Contoh tampilan program sebagai berikut:
